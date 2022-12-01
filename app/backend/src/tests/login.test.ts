ea0ef4ac1ad5ea68a47386cf58ff9b3d3921b38f
Vinícius <viniciusgolfera@gmail.com>
2022-12-01 16:03:43 -0300

    Adicionando testes para avaliar a tentativa de login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -6,7 +6,7 @@ import chaiHttp = require('chai-http');
 
 import App from '../app';
 import User from '../database/models/User';
-import { mockUser, mockLoginBody, mockLoginPassword } from './mocks/login.mock';
+import { mockUser, mockLoginBody } from './mocks/login.mock';
 
 import { Response } from 'superagent';
 
@@ -44,11 +44,49 @@ describe('Testando a rota login', () => {
       chaiHttpResponse = await chai
         .request(app)
         .post('/login')
-        .send(mockLoginPassword);
+        .send({ password: mockLoginBody.password });
   
       expect(chaiHttpResponse.status).to.be.equal(400);
       expect(chaiHttpResponse.body).to.be.haveOwnProperty('message');
       expect(chaiHttpResponse.body.message).to.be.equal('All fields must be filled');
+    });
+
+    it('Se o login falha se faltar a senha', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: mockLoginBody.email });
+  
+      expect(chaiHttpResponse.status).to.be.equal(400);
+      expect(chaiHttpResponse.body).to.be.haveOwnProperty('message');
+      expect(chaiHttpResponse.body.message).to.be.equal('All fields must be filled');
+    });
+
+    it('Se o login falha quando não encontar um email correspondente', async () => {
+      sinon.stub(User, "findOne").resolves(null);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send(mockLoginBody);
+  
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body).to.be.haveOwnProperty('message');
+      expect(chaiHttpResponse.body.message).to.be.equal('Incorrect email or password');
+    });
+
+    it('Se o login falha quando a senha não corresponder', async () => {
+      sinon.stub(User, "findOne").resolves(null);
+      sinon.stub(bcrypt, "compare").resolves(false);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send(mockLoginBody);
+  
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body).to.be.haveOwnProperty('message');
+      expect(chaiHttpResponse.body.message).to.be.equal('Incorrect email or password');
     });
   })
 });
--- app/backend/src/tests/mocks/login.mock.ts
+++ app/backend/src/tests/mocks/login.mock.ts
@@ -3,10 +3,6 @@ export const mockLoginBody = {
   password: 'password',
 };
 
-export const mockLoginPassword = {
-  password: 'password',
-};
-
 export const mockUser = {
   id: 999,
   username: 'User',
