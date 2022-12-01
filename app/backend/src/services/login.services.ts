231139c34fa6c7a6cd0840fae6f1025ef7261f9a
Vinícius <viniciusgolfera@gmail.com>
2022-12-01 16:41:13 -0300

    Criando validações para o login

--- app/backend/src/services/login.services.ts
+++ app/backend/src/services/login.services.ts
@@ -1,18 +1,22 @@
 import * as bcrypt from 'bcryptjs';
-import UserModel from '../database/models/User';
+import UsersModel from '../database/models/User';
 import createToken from '../utils/jwt';
-import { IUserLogin } from '../interfaces/IUser';
+import { IUser, IUserLogin } from '../interfaces/IUser';
 import validatelogin from './validations/validations';
+import HttpException from '../utils/http';
 
 export default class LoginService {
-  private _model = UserModel;
+  private _model = UsersModel;
 
   public async login({ email, password }: IUserLogin): Promise<string> {
+    console.log(email, password);
     validatelogin({ email, password });
 
-    const user = await this._model.findOne({ where: { email } });
+    const user: IUser | null = await this._model.findOne({ where: { email } });
 
-    if (!user || !bcrypt.compare(password, user.password)) return 'token';
+    if (!user || !await bcrypt.compare(password, user.password)) {
+      throw new HttpException(401, 'Incorrect email or password');
+    }
 
     const token = createToken(user);
     return token;
